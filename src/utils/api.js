006389b8b7dda51a1df5eb851426ca7979d88e04
sachinpsgg <sachin@psglobalgroup.com>
2025-04-22 20:07:11 +0530

    backend integrated

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ export const compareFiles = async (file1, file2) => {
     formData.append('file1', file1);
     formData.append('file2', file2);
 
-    const response = await fetch('http://localhost:8000/compare', {
+    const response = await fetch('https://file-compare-backend.onrender.com/compare', {
         method: 'POST',
         body: formData,
     });
